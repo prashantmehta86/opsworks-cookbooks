6cdcc98fab6e99631c2348b5d97adebceb1de9e9
prashantmehta86 <prashant.mehta86@gmail.com>
2015-09-02 15:34:08 -0400

    Update customdatabase.rb

--- mysql/recipes/customdatabase.rb
+++ mysql/recipes/customdatabase.rb
@@ -1,5 +1,5 @@
 node[:deploy].each do |app, deploy|
-  file File.join(deploy[:deploy_to], 'shared', 'config', 'database.yml') do
+  file File.join(deploy[:deploy_to], 'shared', 'config', 'mydatabase.yml') do
     content YAML.dump(node[deploy:][app].to_hash)
   end
 end 
